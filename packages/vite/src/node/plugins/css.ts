46b15c136fb4401f2bad69a4eb087e43d4773eeb
Roc <moonlitusun@gmail.com>
2025-12-29 22:59:00 +0800

    chore: pin esboot

--- packages/vite/src/node/plugins/css.ts
+++ packages/vite/src/node/plugins/css.ts
@@ -223,7 +223,9 @@ export function resolveCSSOptions(
   return resolved
 }
 
-const cssModuleRE = new RegExp(`\\.module${CSS_LANGS_RE.source}`)
+// const cssModuleRE = new RegExp(`\\.module${CSS_LANGS_RE.source}`)
+const cssModuleRE =
+  /\.(?:css|less|sass|scss|styl|stylus|pcss|postcss|sss)\?module(?:&.*)?$/
 const directRequestRE = /[?&]direct\b/
 const htmlProxyRE = /[?&]html-proxy\b/
 const htmlProxyIndexRE = /&index=(\d+)/
